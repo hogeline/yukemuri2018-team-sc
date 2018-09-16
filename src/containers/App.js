10506f300c153ccf787cde2b8882e6cacd586cfa
hogeline <s160708@kushiro.koisen-ac.jp>
2018-09-16 14:53:57 +0900

    Update:src リファクタリング

---
+++ src/containers/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">team sc</h1>
+        </header>
+        <p className="App-intro">hogehoge</p>
+      </div>
+    );
+  }
+}
+
+export default App;
