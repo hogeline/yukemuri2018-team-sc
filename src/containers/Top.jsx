c87754abe9366063421ea34aa911a72f86161214
hogeline <s160708@kushiro.koisen-ac.jp>
2018-09-16 18:08:34 +0900

    Update:TopPage トップ画面のモックを作成

--- src/containers/Top.jsx
+++ src/containers/Top.jsx
@@ -4,13 +4,27 @@ import "./Top.css";
 class Top extends Component {
   render() {
     return (
-      <div className="Top">
-        <div className="top-section">
-          <div className="top-landing">
-            <p>top page</p>
+      <div className="top">
+        <div className="top-landing">
+          <div className="top-landing-logo">ロゴ</div>
+          <div className="top-landing-title">
+            <h1>
+              自分のスキルに
+              <br />
+              合った質問を探す
+            </h1>
           </div>
-          <div className="top-search">
-            <p>hogehoge</p>
+          <div className="top-landing-source">
+            <h2>For Teratail</h2>
+          </div>
+        </div>
+        <div className="top-search">
+          <div className="top-search-title">
+            <h2>検索条件</h2>
+          </div>
+          <div className="container">
+            <div className="top-search-conditions">条件</div>
+            <div className="top-search-btn">検索</div>
           </div>
         </div>
       </div>
